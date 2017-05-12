4028397a248d608c83317f0d75f0d3dee9b44c5a
LuckyGan <18357154046@163.com>
2017-05-12 09:23:58 +0800

    update code struct

---
+++ SIMS/constInfo.h
@@ -0,0 +1,21 @@
+#ifndef CONST_INFO
+#define CONST_INFO
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+#include<ctype.h>
+#define TITLE_LEN 100
+#define TITLE_WID 2
+#define USER_INFO_TITLE_BEGIN TITLE_LEN / 8
+#define USER_ACCOUNT_TITLE_BEGIN TITLE_LEN / 3
+#define USER_NAME_LEN 20
+#define USER_PWD_LEN 20
+//
+
+char* PROJECT_NAME;
+char* MANAGER_MENU[9];
+char* USER_MENU[3];
+
+#endif // CONST_INFO
+
---
+++ SIMS/dataStruct.h
@@ -0,0 +1,19 @@
+#ifndef DATA_STRUCT
+#define DATA_STRUCT
+#include "constInfo.h"
+typedef struct userAccountStruct {//用户账号结构体
+	char userName[USER_NAME_LEN];
+	char userPwd[USER_PWD_LEN];
+	int userRole;
+	struct userAccountStruct* next;
+}userAccountStruct, *pUserAccountStruct;
+typedef struct studentInfoStruct {//学生信息结构体
+	int userId;
+	char userName[USER_NAME_LEN];
+	float userCourse1Score;
+	float userCourse2Score;
+	float userCourse3Score;
+	struct studentInfoStruct* next;
+}studentInfoStruct, *pStudentInfoStruct;
+#endif // DATA_STRUCT
+
